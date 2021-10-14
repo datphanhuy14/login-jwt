15908d12eb6c3ece18c557c177e9680526683325
Dat Phan Huy <datph.zes@gmail.com>
2021-10-14 18:24:00 +0700

    update

--- src/app/middlewares/passport.js
+++ src/app/middlewares/passport.js
@@ -1,60 +1,77 @@
-const passport = require("passport");
-const FacebookStrategy = require("passport-facebook").Strategy;
-const config = require("../config/db.config");
-const GoogleStrategy = require("passport-google-oauth2").Strategy;
-const db = require("../models");
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
+const config = require('../config/db.config');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const db = require('../models');
+// const {jwtHelper} = require('../helpers')
+
 
 passport.serializeUser(function(user, cb) {
-    console.log(user);
-    cb(null, user);
-  });
-  passport.deserializeUser(function(user, cb) {
-      console.log('aa',user)
-    cb(null, user);
-  });
-    passport.use(
-      new FacebookStrategy(
+  console.log(user);
+  cb(null, user);
+});
+passport.deserializeUser(function(user, cb) {
+  cb(null, user);
+});
+passport.use(
+    new FacebookStrategy(
         {
           clientID: config.clientID,
           clientSecret: config.clientSecret,
           callbackURL: config.callbackURL,
-          profileFields: ["email", "gender", "locale", "displayName"],
+          profileFields: ['email', 'gender', 'locale', 'displayName'],
         },
-        function (accessToken, refreshToken, profile, cb) {
-          // console.log(profile);
-          return cb(null, profile);
-        }
-      )
-    );
-  
-    passport.use(
-      new GoogleStrategy(
+        function(request, accessToken, refreshToken, profile, cb) {
+          process.nextTick(function() {
+            db.users
+                .findOrCreate({
+                  where: {
+                    facebookId: profile.id,
+                  },
+                  defaults: {
+                    email: profile._json.email,
+                    fullname: profile.displayName,
+                    // user_fullName: profile.displayName,
+                    facebookId: profile.id,
+                  },
+                })
+                .then((user) => {
+                  request.session.user = user;
+                  return cb(null, user[0]);
+                });
+          });
+        },
+    ),
+);
+
+passport.use(
+    new GoogleStrategy(
         {
           clientID: config.gClientID,
           clientSecret: config.gClientSecret,
           callbackURL: config.gCallbackURL,
           passReqToCallback: true,
         },
-        function (request, accessToken, refreshToken, profile, cb) {
-          process.nextTick(function () {
+        function(request, accessToken, refreshToken, profile, cb) {
+          process.nextTick(function() {
             db.users
-              .findOrCreate({
-                where: {
-                  googleId: profile.id,
-                },
-                defaults: {
-                  email: profile._json.email,
-                  fullname: profile.displayName,
-                  // user_fullName: profile.displayName,
-                  googleId: profile.id,
-                },
-              })
-              .then((user) => {
-                request.session.user  = user;
-                return cb(null, user[0]);
-              });
+                .findOrCreate({
+                  where: {
+                    googleId: profile.id,
+                  },
+                  defaults: {
+                    email: profile._json.email,
+                    fullname: profile.displayName,
+                    // user_fullName: profile.displayName,
+                    googleId: profile.id,
+                  },
+                })
+                .then((user) => {
+                  request.session.user = user;
+                  return cb(null, user[0]);
+                });
           });
-        }
-      )
-    );
-  
+        },
+    ),
+);
+
--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const loginApi = require('./user');
 const mainApi = require('./apiAuth');
-const {AuthMiddleWare} = require("../middlewares/");
+const {AuthMiddleWare} = require('../middlewares/');
 
 /* GET home page. */
-router.use('/auth', loginApi)
-router.use('/api',AuthMiddleWare.isAuth, mainApi)
+router.use('/auth', loginApi);
+router.use('/api', AuthMiddleWare.isAuth, mainApi);
 
 module.exports = router;
