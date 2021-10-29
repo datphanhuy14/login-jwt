3f9feb48f59b0c968a58e7f630f5d8f02d785d04
Dat Phan Huy <datph.zes@gmail.com>
2021-10-29 18:28:28 +0700

    subject

--- src/migrations/20211028110920-create-update-by.js
+++ src/migrations/20211028110920-create-update-by.js
@@ -4,6 +4,8 @@ module.exports = {
     up: async (queryInterface, Sequelize) => {
         await queryInterface.addColumn('courses','updated_by',{type: Sequelize.DataTypes.UUID});
         await queryInterface.addColumn('courses','created_by',{type: Sequelize.DataTypes.UUID});
+        await queryInterface.addColumn('subjects','updated_by',{type: Sequelize.DataTypes.UUID});
+        await queryInterface.addColumn('subjects','created_by',{type: Sequelize.DataTypes.UUID});
         await queryInterface.addColumn('lessons','updated_by',{type: Sequelize.DataTypes.UUID});
         await queryInterface.addColumn('lessons','created_by',{type: Sequelize.DataTypes.UUID});
         await queryInterface.addColumn('questions','updated_by',{type: Sequelize.DataTypes.UUID});
@@ -20,6 +22,8 @@ module.exports = {
     down: async (queryInterface, Sequelize) => {
         await queryInterface.removeColumn('courses','updated_by');
         await queryInterface.removeColumn('courses','created_by');
+        await queryInterface.removeColumn('subjects','updated_by');
+        await queryInterface.removeColumn('subjects','created_by');
         await queryInterface.removeColumn('lessons','updated_by');
         await queryInterface.removeColumn('lessons','created_by');
         await queryInterface.removeColumn('questions','updated_by');
